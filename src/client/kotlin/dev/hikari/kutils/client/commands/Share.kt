7b333b1f951170a0aa194dd79cadd290a8fc8dfe
CattoYT <twilsprk@gmail.com>
2024-12-08 20:47:10 +0000

    share to chat partially implemented

--- src/client/kotlin/dev/hikari/kutils/client/commands/Share.kt
+++ src/client/kotlin/dev/hikari/kutils/client/commands/Share.kt
@@ -5,8 +5,13 @@ import dev.hikari.kutils.client.KutilsClient
 import kotlinx.coroutines.runBlocking
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.client.MinecraftClient
+import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.text.ClickEvent
+import net.minecraft.text.Style
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 
 class Share {
 
@@ -26,9 +31,17 @@ class Share {
     fun shareSpotify() {
         KutilsClient.logger.info("Share Spotify command executed")
         runBlocking {
-            var currentlyPlaying = KutilsClient.Spotify.spotifyApi?.player?.getCurrentlyPlaying()?.item?.href
-
-            MinecraftClient.getInstance().player?.networkHandler?.sendChatCommand(currentlyPlaying); //replace with chat message
+            var currentlyPlaying = KutilsClient.Spotify.spotifyApi?.player?.getCurrentlyPlaying()?.item?.asTrack?.href.toString().replace("https://api.spotify.com/v1/tracks/", "https://open.spotify.com/track/")
+            println(currentlyPlaying)
+            val clickableText = Text.literal("Click me to link Spotify!")
+                .setStyle(
+                    Style.EMPTY
+                        .withColor(Formatting.AQUA)
+                        .withItalic(true)
+                        .withUnderline(true)
+                        .withClickEvent(ClickEvent(ClickEvent.Action.OPEN_URL, currentlyPlaying))
+                )
+            MinecraftClient.getInstance().getNetworkHandler()?.sendPacket(ChatMessageC2SPacket("/say Hello from MyFabricMod!"))
         }
     }
 }
